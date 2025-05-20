7e89cd3e52cb8baf11044c1abe15d2b39db9e497
Kamal Majaiti <kmajaiti011j@fpdrioja.com>
2025-05-20 12:07:23 +0000

    Agregar documentacion y arreglar codigo

--- demo.php
+++ demo.php
@@ -1,14 +1,38 @@
 <?php 
+/**
+ * Verificador de email en PHP
+ *
+ * Codigo en PHP demo para el examen de mayo de  DAW, para GIT.
+ *
+ * @author    Kamal Majaiti El Majaiti
+ * @copyright © 2025 Kamal Majaiti El Majaiti. Licenciado bajo CC BY-SA 4.0
+ * @license   https://creativecommons.org/licenses/by-sa/4.0/ Creative Commons Attribution-ShareAlike 4.0
+ * @link      https://github.com/kmajaiti011j/mayo25
+ * @version   1.0
+ * @since     PHP 5.6
+ */
+
+
+/**
+ * Funcion que procesa el formulario de Email de solicitud HTTP POST.
+ *
+ * Esta función permite realizar probar el email que se ha recibido por POST al servidor web.
+ *
+ * @example
+ * procesarEmailFormulario();
+ * die();
+ */
+
+
 function procesarEmailFormulario() {
 	$email=$_POST['email'] ?? '';
 
 	 if ($email) {
-		echo "Email '". htmlspecialchars($email) . "' recibido con éxito (no se guarda)
+		echo 'Email '. htmlspecialchars($email) . ' recibido con éxito (no se guarda)';
 		} 
 	else {
-		echo "Por favor, envia un email en el parametro $email via POST. (ejemplo: kml@kml.com)";
+		echo 'Por favor, envia un email en el parametro $email via POST. (ejemplo: kml@kml.com)';
 		}
-	}
 }
 
 if ($_SERVER["REQUEST_METHOD"=='POST']) {
---
+++ documentacion/js/searchIndex.js
@@ -0,0 +1,14 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\procesarEmailFormulario\u0028\u0029",
+            "name": "procesarEmailFormulario",
+            "summary": "Funcion\u0020que\u0020procesa\u0020el\u0020formulario\u0020de\u0020Email\u0020de\u0020solicitud\u0020HTTP\u0020POST.",
+            "url": "namespaces/default.html#function_procesarEmailFormulario"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
